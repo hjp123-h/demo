c1bde7b773cbe4a479e906693024c6e23beba2f7
hjp123-h <hjp1960883803@icloud.com>
2020-03-01 15:42:18 +0800

    增加了多环境配置主页登陆修改

--- src/main/java/com/hjp123/demo/controller/AuthorizeConteoller.java
+++ src/main/java/com/hjp123/demo/controller/AuthorizeConteoller.java
@@ -4,9 +4,12 @@ import com.hjp123.demo.dto.AccessTokenDTO;
 import com.hjp123.demo.dto.GithubUser;
 import com.hjp123.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeConteoller {
@@ -18,20 +21,38 @@ public class AuthorizeConteoller {
      * 用来向接收Github发送过来的用户密钥
      */
 
+    @Value("${Github.setClient.id}")
+    private String Clientid;
+    @Value("${Github.setClient.secret}")
+    private String Secret;
+    @Value("${Github.setRedirect.uri}")
+    private String Uri;
+
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code,@RequestParam(name = "state") String state){
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("73ea771286d3bb60c202");
-        accessTokenDTO.setClient_secret("4246ea5f2d94357467ffc070898740d8309d2273");
+        accessTokenDTO.setClient_id(Clientid);
+        accessTokenDTO.setClient_secret(Secret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8088/callback");
+        accessTokenDTO.setRedirect_uri(Uri);
         accessTokenDTO.setState(state);
         System.out.println(accessTokenDTO);
         //提交密钥
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         //解析用户数据
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName()+"---"+user.getId());
-        return "index";
+        System.out.println(user.getName()+"====="+user.getId());
+        if (user != null){
+            //登陆成功
+            request.getSession().setAttribute("user",user);
+            System.out.println("成功");
+            return "redirect:/";
+        }else{
+            //登陆失败
+            System.out.println("失败");
+            return "redirect:/";
+        }
     }
 }
--- src/main/java/com/hjp123/demo/controller/HelloController.java
+++ src/main/java/com/hjp123/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.hjp123.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
+    @Value("${Github.setClient.id}")
+    private String Clientid;
+    @Value("${Github.setRedirect.uri}")
+    private String uri;
+
     @GetMapping("/")
     public String hello(Model model){
 
+        model.addAttribute("uri",uri);
+        model.addAttribute("clientid",Clientid);
         return "index";
     }
 }
